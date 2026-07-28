4aa453e0012299081384e1744eeb42b83158f6f9
Kai Ko <53098781+kenduck1@users.noreply.github.com>
2026-07-28 01:18:31 -0400

    Add Home/Editor screen stubs, wire App.tsx routing, remove demo scaffold

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,11 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { useAppStore } from './store/appStore'
+import HomeScreen from './screens/HomeScreen'
+import EditorScreen from './screens/EditorScreen'
 
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const screen = useAppStore((state) => state.screen)
 
-  return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+  return screen === 'home' ? <HomeScreen /> : <EditorScreen />
 }
 
 export default App
