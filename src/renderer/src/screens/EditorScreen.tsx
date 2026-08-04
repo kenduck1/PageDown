158ec4913eed0558c2468401b56ccb9707cd2dbd
Kai Ko <53098781+kenduck1@users.noreply.github.com>
2026-08-04 05:22:27 -0400

    Mount MilkdownEditor as EditorScreen's real editing surface

--- src/renderer/src/screens/EditorScreen.tsx
+++ src/renderer/src/screens/EditorScreen.tsx
@@ -1,11 +1,13 @@
 import { useAppStore } from '../store/appStore'
 import { useDocumentStore } from '../store/documentStore'
+import MilkdownEditor from '../milkdown/MilkdownEditor'
 
 function EditorScreen(): React.JSX.Element {
   const goHome = useAppStore((state) => state.goHome)
-  const viewMode = useAppStore((state) => state.viewMode)
   const filePath = useDocumentStore((state) => state.filePath)
   const content = useDocumentStore((state) => state.content)
+  const revision = useDocumentStore((state) => state.revision)
+  const updateContent = useDocumentStore((state) => state.updateContent)
   const error = useDocumentStore((state) => state.error)
   const clearError = useDocumentStore((state) => state.clearError)
   const save = useDocumentStore((state) => state.save)
@@ -29,13 +31,13 @@ function EditorScreen(): React.JSX.Element {
           </button>
         </div>
       )}
-      <div className="flex-1 overflow-auto p-4">
-        <p className="mb-2 text-12 text-text-tertiary">
-          View mode: {viewMode} -- full editor chrome built in sub-projects #3-7
-        </p>
-        <pre data-testid="document-content" className="whitespace-pre-wrap font-mono text-12-5">
-          {content}
-        </pre>
+      <div data-testid="document-content" className="flex-1 overflow-auto">
+        <MilkdownEditor
+          key={revision}
+          content={content}
+          onChange={updateContent}
+          onError={(message) => useDocumentStore.setState({ error: message })}
+        />
       </div>
     </div>
   )
